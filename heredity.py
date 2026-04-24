bc901d78407fc19336203dfdf18c8b56c40c50b2
gituser-sudo <48635872+gituser-sudo@users.noreply.github.com>
2026-04-24 21:39:22 +0000

    Fri, Apr 24, 2026, 4:39 PM -05:00

--- heredity.py
+++ heredity.py
@@ -261,7 +261,7 @@ def update(probabilities, one_gene, two_genes, have_trait, p):
     Which value for each distribution is updated depends on whether
     the person is in `have_gene` and `have_trait`, respectively.
     """
-    raise NotImplementedError
+
 
 
 def normalize(probabilities):
